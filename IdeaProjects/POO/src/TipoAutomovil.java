4d113d361f1e28417183bcd52a9b9d2fb46a809f
16091221 <67929833+16091221@users.noreply.github.com>
2021-01-20 00:50:38 -0600

    Sección 14: Programación orientada a objetos: Clases y objetos

---
+++ IdeaProjects/POO/src/TipoAutomovil.java
@@ -0,0 +1,32 @@
+public enum TipoAutomovil {
+    SEDAN("Sedan", "Auto normal", 4),
+    STATION_WAGON("Station Wagon", "Auto grande", 5),
+    HATCHBACK("Hatchback", "Auto compacto", 5),
+    PICKUP("Pickup", "Camioneta", 4),
+    COUPE("Coupe", "Auto deportivo", 2),
+    CONVERTIBLE("Convertible", "Auto deportivo", 2),
+    FURGON("Furgón", "Auto utilitario", 3),
+    SUV("SUV", "Todo terreno deportivo", 5);
+
+    private final String nombre;
+    private final int numeroPuertas;
+    private final String descripcion;
+
+    TipoAutomovil(String nombre, String descripcion, int numeroPuertas) {
+        this.nombre = nombre;
+        this.numeroPuertas = numeroPuertas;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getNumeroPuertas() {
+        return numeroPuertas;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+}
