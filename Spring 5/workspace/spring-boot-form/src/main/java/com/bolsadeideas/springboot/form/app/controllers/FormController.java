3bd4c60cda07d2305b367889997374d82fa9bb88
16091221 <67929833+16091221@users.noreply.github.com>
2021-01-25 21:12:17 -0600

    Sección 40: Spring Framework 5: Interceptors HTTP

--- Spring 5/workspace/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ Spring 5/workspace/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -126,7 +126,7 @@ public class FormController {
 		Usuario usuario = new Usuario();
 		usuario.setNombre("John");
 		usuario.setApellido("Doe");
-		usuario.setIdentificador("123.456.789-K");
+		usuario.setIdentificador("12.456.789-K");
 		usuario.setHabilitar(true);
 		usuario.setValorSecreto("Algun valor secreto");
 		usuario.setPais(new Pais(2, "MX","México"));
