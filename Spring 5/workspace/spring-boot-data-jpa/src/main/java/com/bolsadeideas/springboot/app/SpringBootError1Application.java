ec4c475a5ede9c62fff7022664356edb9a45e2a2
16091221 <67929833+16091221@users.noreply.github.com>
2021-01-26 14:47:20 -0600

    Sección 43:Spring Framework 5: CRUD usando Spring Data JPA

---
+++ Spring 5/workspace/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/SpringBootError1Application.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootError1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootError1Application.class, args);
+	}
+
+}
