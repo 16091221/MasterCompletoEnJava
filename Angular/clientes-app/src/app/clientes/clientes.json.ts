b95f5a592b80de8221983db5ba479de8d48d5936
16091221 <67929833+16091221@users.noreply.github.com>
2021-01-26 22:22:40 -0600

    Sección 46: Angular & Spring 5: Componente clientes

---
+++ Angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,74 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {
+    id: 1,
+    nombre: "Pearl",
+    apellido: "Albert",
+    email: "pearlalbert@roughies.com",
+    createAt: "2015-06-18"
+  },
+  {
+    id: 2,
+    nombre: "Eliza",
+    apellido: "Stephenson",
+    email: "elizastephenson@roughies.com",
+    createAt: "2019-11-30"
+  },
+  {
+    id: 3,
+    nombre: "Shaw",
+    apellido: "Delaney",
+    email: "shawdelaney@roughies.com",
+    createAt: "2016-04-29"
+  },
+  {
+    id: 4,
+    nombre: "Sabrina",
+    apellido: "Rasmussen",
+    email: "sabrinarasmussen@roughies.com",
+    createAt: "2017-01-16"
+  },
+  {
+    id: 5,
+    nombre: "Nunez",
+    apellido: "Barron",
+    email: "nunezbarron@roughies.com",
+    createAt: "2014-03-31"
+  },
+  {
+    id: 6,
+    nombre: "Barr",
+    apellido: "Romero",
+    email: "barrromero@roughies.com",
+    createAt: "2014-05-10"
+  },
+  {
+    id: 7,
+    nombre: "Carmela",
+    apellido: "Cabrera",
+    email: "carmelacabrera@roughies.com",
+    createAt: "2020-04-11"
+  },
+  {
+    id: 8,
+    nombre: "Mindy",
+    apellido: "Cook",
+    email: "mindycook@roughies.com",
+    createAt: "2015-05-12"
+  },
+  {
+    id: 9,
+    nombre: "Petty",
+    apellido: "Boyer",
+    email: "pettyboyer@roughies.com",
+    createAt: "2020-06-12"
+  },
+  {
+    id: 10,
+    nombre: "Cash",
+    apellido: "Hayden",
+    email: "cashhayden@roughies.com",
+    createAt: "2018-06-06"
+  }
+]
